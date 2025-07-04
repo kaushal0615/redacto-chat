e88d5842460e6d5eb77366bec9096ead1daf090a
Kaushal Pancholi <kaushal.pancholi@iiitb.ac.in>
2025-07-04 08:00:00 +0530

    updated asgi.py and models.py

--- redacto_chat/asgi.py
+++ redacto_chat/asgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
-
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+import chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'redacto_chat.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_backend.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(chat.routing.websocket_urlpatterns)
+    ),
+})
